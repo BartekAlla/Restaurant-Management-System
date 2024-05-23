5c7fd000e1414f4028050300c896ecf7a36610a2
Bartlomiej <b.allaburda@gmail.com>
2024-05-23 18:46:50 +0200

    Init

---
+++ Backend/src/test/java/pl/prg/ba/restaurantmanagementbackend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.prg.ba.restaurantmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
