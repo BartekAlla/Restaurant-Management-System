493dcf7c23c98d5b2a968fda97831ce600e6fef3
Bartlomiej <b.allaburda@gmail.com>
2024-05-30 19:17:12 +0200

    Added unique property to menu entity name

--- Backend/src/main/java/pl/prg/ba/restaurantmanagementbackend/menuManagement/entity/menu/Menu.java
+++ Backend/src/main/java/pl/prg/ba/restaurantmanagementbackend/menuManagement/entity/menu/Menu.java
@@ -19,7 +19,7 @@ public class Menu {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @Column(nullable = false)
+    @Column(nullable = false, unique = true)
     private String name;
     @ManyToMany
     @JoinTable(
