73e279e65a596c5be8ffdb3e87c9455d2328b15e
Bartlomiej <b.allaburda@gmail.com>
2024-05-30 16:46:33 +0200

    Added IngredientDTO unit tests

--- Backend/src/main/java/pl/prg/ba/restaurantmanagementbackend/menuManagement/dto/ingredient/IngredientDTO.java
+++ Backend/src/main/java/pl/prg/ba/restaurantmanagementbackend/menuManagement/dto/ingredient/IngredientDTO.java
@@ -9,10 +9,25 @@ import lombok.Setter;
 //TODO
 //  Unit Tests for IngredientDTO
 @Getter
-@Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class IngredientDTO {
-        private String name;
+    private String name;
 
+    public IngredientDTO(String name) {
+        validateIngredientDTOName(name);
+    }
+
+    private void validateIngredientDTOName(String name) throws NullPointerException, IllegalArgumentException {
+        if (name == null) {
+            throw new NullPointerException("Name cannot be null");
+        } else if (name.isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty String");
+        } else if (name.isBlank()) {
+            throw new IllegalArgumentException("Name cannot contain only white spaces");
+        }
+        this.name = name;
+    }
+    public void setName(String name) {
+        validateIngredientDTOName(name);
+    }
 }
---
+++ Backend/src/main/java/pl/prg/ba/restaurantmanagementbackend/menuManagement/dto/menu/MenuDTO.java
@@ -0,0 +1,4 @@
+package pl.prg.ba.restaurantmanagementbackend.menuManagement.dto.menu;
+
+public class MenuDTO {
+}
