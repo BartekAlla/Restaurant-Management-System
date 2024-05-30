1baebbdfd77b5b80095a4239c00410e2da2eb0b0
Bartlomiej <b.allaburda@gmail.com>
2024-05-30 16:51:33 +0200

    Updated main branch

--- Backend/src/main/java/pl/prg/ba/restaurantmanagementbackend/menuManagement/controller/ingredient/IngredientController.java
+++ Backend/src/main/java/pl/prg/ba/restaurantmanagementbackend/menuManagement/controller/ingredient/IngredientController.java
@@ -2,10 +2,7 @@ package pl.prg.ba.restaurantmanagementbackend.menuManagement.controller.ingredie
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.prg.ba.restaurantmanagementbackend.menuManagement.dto.ingredient.IngredientDTO;
 import pl.prg.ba.restaurantmanagementbackend.menuManagement.service.ingredient.IngredientService;
 
@@ -15,9 +12,6 @@ public class IngredientController {
 
     @Autowired
     private IngredientService ingredientService;
-    //TODO
-    // add new endpoints
-    // for new methods
 
     @PostMapping
     public ResponseEntity<?> addIngredient(@RequestBody IngredientDTO ingredientDTO) {
