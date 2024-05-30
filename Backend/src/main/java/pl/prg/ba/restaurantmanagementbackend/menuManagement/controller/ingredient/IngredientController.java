47dee63257deed26f397f32d6a5e80e58ed1ded0
Bartlomiej <b.allaburda@gmail.com>
2024-05-30 15:58:13 +0200

    Added new endpoints for Ingredient Entity

--- Backend/src/main/java/pl/prg/ba/restaurantmanagementbackend/menuManagement/controller/ingredient/IngredientController.java
+++ Backend/src/main/java/pl/prg/ba/restaurantmanagementbackend/menuManagement/controller/ingredient/IngredientController.java
@@ -2,10 +2,7 @@ package pl.prg.ba.restaurantmanagementbackend.menuManagement.controller.ingredie
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import pl.prg.ba.restaurantmanagementbackend.menuManagement.dto.ingredient.IngredientDTO;
 import pl.prg.ba.restaurantmanagementbackend.menuManagement.entity.ingredient.Ingredient;
 import pl.prg.ba.restaurantmanagementbackend.menuManagement.service.ingredient.IngredientService;
@@ -28,24 +25,27 @@ public class IngredientController {
         ingredientService.addIngredient(ingredientDTO);
         return ResponseEntity.ok().build();
     }
+    @GetMapping
+    public ResponseEntity<List<Ingredient>> getAllIngredients() {
+        List<Ingredient> ingredients = ingredientService.getAllIngredients();
+        return ResponseEntity.ok(ingredients);
+    }
 
-//    public List<Ingredient> getAllIngredients() {
-//
-//    }
-//
-//
-//    public Optional<Ingredient> getIngredientById(Long id) {
-//
-//    }
-//
-//
-//    public Ingredient updateIngredient(Long id, IngredientDTO ingredientDTO) {
-//
-//    }
-//
-//
-//    public void deleteIngredient(Long id) {
-//
-//    }
+    @GetMapping("/{id}")
+    public ResponseEntity<Ingredient> getIngredientById(@PathVariable Long id) {
+        return ingredientService.getIngredientById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+    @PutMapping("/{id}")
+    public ResponseEntity<Ingredient> updateIngredient(@PathVariable Long id, @RequestBody IngredientDTO ingredientDTO) {
+        Ingredient updatedIngredinet = ingredientService.updateIngredient(id, ingredientDTO);
+        return ResponseEntity.ok(updatedIngredinet);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteIngredientById(@PathVariable Long id) {
+        ingredientService.deleteIngredient(id);
+        return ResponseEntity.noContent().build();
+    }
 
 }
