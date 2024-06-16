f70a576c6c64d530446c146176b150383f8807f1
Bartlomiej <b.allaburda@gmail.com>
2024-06-16 13:35:01 +0200

    Layout.js edits

--- Frontend/src/components/NavBar.js
+++ Frontend/src/components/NavBar.js
@@ -4,14 +4,23 @@ import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
 import { Link } from 'react-router-dom';
-//TODO - navBar must be inside of Shape not a seperate part of website
+import logo from './logoNavBar.png';
+
 const NavBar = () => {
     return (
-        <Box sx={{ flexGrow: 1 }}>
-            <AppBar position="static">
-                <Toolbar>
-                    <Button color="inherit" component={Link} to="/">Home page</Button>
-                    <Button color="inherit" component={Link} to="/menu">Menus</Button>
+        <Box>
+            <AppBar position="fixed" style={{ backgroundColor: '#f5f3ea', color: '#1c2c4b', top: "2.6%", height: '10.5%' }}>
+                <Toolbar sx={{ display: 'flex', justifyContent: 'space-between', paddingLeft: '5%'}}>
+                    <Box sx={{ display: 'flex', alignItems: 'center', height: '100%' }}>
+                        <Button component={Link} to="/">
+                            <img src={logo} alt="Logo" style={{ maxHeight: '10%', maxWidth: '100%', height: 'auto' }} />
+                        </Button>
+                    </Box>
+                    <Box sx={{ display: 'flex', alignItems: 'center' , paddingRight: '7%' }}>
+                        <Button color="inherit" component={Link} to="/menus">MENUS</Button>
+                        <Button color="inherit" component={Link} to="/story">OUR STORY</Button>
+                        <Button color="inherit" component={Link} to="/reservation">RESERVATIONS</Button>
+                    </Box>
                 </Toolbar>
             </AppBar>
         </Box>
