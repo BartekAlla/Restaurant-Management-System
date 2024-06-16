1f67acf9548442544e4150abe5619b731889f2b2
Bartlomiej <b.allaburda@gmail.com>
2024-06-16 17:14:06 +0200

    Added TODO

--- Frontend/src/components/Layout/Layout.js
+++ Frontend/src/components/Layout/Layout.js
@@ -4,6 +4,9 @@ import Shape from '../Shape/Shape';
 import Box from '@mui/material/Box';
 import { makeStyles } from '@mui/styles';
 
+
+//TODO ELEVATE NAV BAR WHILE SCROLLING DOWN
+
 const useStyles = makeStyles(() => ({
     layoutContainer: {
         height: '100vh',
