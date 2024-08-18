c533f54298edb55034d25889d704c48d9fdff774
balla <b.allaburda@gmail.com>
2024-08-18 18:49:40 +0200

    Updated validation method for Menu and Ingredient DTO's

--- Backend/src/main/java/pl/prg/ba/restaurantmanagementbackend/menuManagement/dto/menu/MenuDTO.java
+++ Backend/src/main/java/pl/prg/ba/restaurantmanagementbackend/menuManagement/dto/menu/MenuDTO.java
@@ -3,6 +3,7 @@ package pl.prg.ba.restaurantmanagementbackend.menuManagement.dto.menu;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import pl.prg.ba.restaurantmanagementbackend.common.Validators;
 
 @Getter
 @NoArgsConstructor
@@ -13,13 +14,7 @@ public class MenuDTO {
     }
 
     private void validateMenuDTOName(String name) throws NullPointerException, IllegalArgumentException {
-        if (name == null) {
-            throw new NullPointerException("Name cannot be null");
-        } else if (name.isEmpty()) {
-            throw new IllegalArgumentException("Name cannot be empty String");
-        } else if (name.isBlank()) {
-            throw new IllegalArgumentException("Name cannot contain only white spaces");
-        }
+        Validators.validateName(name);
         this.name = name;
     }
     public void setName(String name) {
