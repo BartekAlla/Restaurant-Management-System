9de4395f4c208047457d4bbe2694c3f847c6be32
balla <b.allaburda@gmail.com>
2024-09-28 14:21:35 +0200

    Added catyegoryDTO UNIT tests

---
+++ Backend/src/test/java/pl/prg/ba/restaurantmanagementbackend/menuManagement/category/MenuCategoryDTOTest.java
@@ -0,0 +1,39 @@
+package pl.prg.ba.restaurantmanagementbackend.menuManagement.category;
+
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import pl.prg.ba.restaurantmanagementbackend.menuManagement.dto.category.CategoryDTO;
+import pl.prg.ba.restaurantmanagementbackend.menuManagement.entity.category.MenuCategoryType;
+
+import static org.hibernate.validator.internal.util.Contracts.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class MenuCategoryDTOTest {
+    private MenuCategoryType menuCategoryType;
+    private CategoryDTO categoryDTO;
+
+    @BeforeEach
+    public void setUp() {
+        menuCategoryType = MenuCategoryType.MAIN;
+        categoryDTO = new CategoryDTO(menuCategoryType);
+    }
+
+    @Test
+    public void testValidCategoryDTOCreation() {
+        assertNotNull(categoryDTO);
+        assertEquals(menuCategoryType, categoryDTO.getMenuCategoryType());
+    }
+    @Test
+    public void testSettingValidCategoryType() {
+        MenuCategoryType newMenuCategoryType = MenuCategoryType.MAIN;
+        categoryDTO.setMenuCategoryType(newMenuCategoryType);
+        assertEquals(newMenuCategoryType, categoryDTO.getMenuCategoryType());
+    }
+    @Test
+    public void testSettingNullCategoryType() {
+        Exception exception = assertThrows(NullPointerException.class, () -> categoryDTO.setMenuCategoryType(null));
+        assertEquals("Menu category type cannot be null", exception.getMessage());
+    }
+}
