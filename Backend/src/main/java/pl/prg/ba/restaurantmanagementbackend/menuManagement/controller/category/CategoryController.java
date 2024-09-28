3792c17bc29620271334ecaee3d95986fa13705e
balla <b.allaburda@gmail.com>
2024-09-28 14:08:25 +0200

    Finished category controller

--- Backend/src/main/java/pl/prg/ba/restaurantmanagementbackend/menuManagement/controller/category/CategoryController.java
+++ Backend/src/main/java/pl/prg/ba/restaurantmanagementbackend/menuManagement/controller/category/CategoryController.java
@@ -14,14 +14,36 @@ import java.util.List;
 public class CategoryController {
     @Autowired
     private CategoryService categoryService;
+
     @PostMapping
     public ResponseEntity<MenuCategory> addCategory(@RequestBody CategoryDTO categoryDTO) {
         MenuCategory menuCategory = categoryService.addMenuCategory(categoryDTO);
         return ResponseEntity.ok(menuCategory);
     }
+
     @GetMapping
     public ResponseEntity<List<MenuCategory>> getAllMenuCategories() {
         List<MenuCategory> menuCategories = categoryService.getAllMenuCategories();
         return ResponseEntity.ok(menuCategories);
     }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<MenuCategory> getMenuCategoryById(@PathVariable Long id) {
+        return categoryService.getMenuCategoryById(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<MenuCategory> updateMenuCategory(@PathVariable Long id, @RequestBody CategoryDTO categoryDTO) {
+        MenuCategory updatedCategory = categoryService.updateMenuCategory(id, categoryDTO);
+        return ResponseEntity.ok(updatedCategory);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteMenuCategoryById(@PathVariable Long id) {
+        categoryService.deleteMenuCategory(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
